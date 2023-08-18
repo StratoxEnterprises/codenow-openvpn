25a4c14edebf9da822d97102134aa97004c7ef2b
Milan Bednar <milan.bednar@stratox.cz>
2023-08-18 18:45:34 +0200

    Doplneni vpn konfigurace a README.md, zacisteni Dockerfile a run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM alpine:3.18
-LABEL maintainer="bdnr"
 
 WORKDIR /app
 
-RUN apk add --update --no-cache bash openvpn iptables tcpdump busybox-extras curl
+RUN apk add --update --no-cache openvpn iptables curl # bash tcpdump busybox-extras
 
-RUN echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/ipv4.conf
 COPY run.sh .
 
 CMD ["/app/run.sh"]
--- run.sh
+++ run.sh
@@ -2,16 +2,14 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
-echo "tun" >> /etc/modules-load.d/tun.conf
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/ipv4.conf
 
+echo "tun" >> /etc/modules-load.d/tun.conf
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# route HTTPS traffic to reverse proxy in private network
 iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 443 -j DNAT --to-destination <HERE FILL PRIVATE IP ADDRESS>:443
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
-sysctl -p /etc/sysctl.d/ipv4.conf
-
-
 openvpn --config /codenow/config/vpn.conf --askpass /codenow/config/pass
-
